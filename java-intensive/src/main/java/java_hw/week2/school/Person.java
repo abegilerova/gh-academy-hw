d2aca0c34f303ff1aef267b688d0b8a5871ca1f7
Aikanysh Begilerova <abegilerova@gmail.com>
2024-10-08 16:27:25 -0500

    week2 hw

---
+++ java-intensive/src/main/java/java_hw/week2/school/Person.java
@@ -0,0 +1,19 @@
+package java_hw.week2.school;
+
+public class Person {
+
+    public String name;
+    protected int age;
+
+    public Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public void displayDetails(){
+        System.out.println("Name " + name);
+        System.out.println("Age " + age);
+    }
+
+
+}
---
+++ java-intensive/src/main/java/java_hw/week2/school/managament/Student.java
@@ -0,0 +1,34 @@
+package java_hw.week2.school.managament;
+
+import java_hw.week2.school.Person;
+
+import java.util.List;
+
+public class Student extends Person {
+
+    private int studentID;
+    private List <String> enrolledCourses;
+
+    public double getTuition() {
+        return tuition;
+    }
+
+    public void setTuition(double tuition) {
+        this.tuition = enrolledCourses.size() * 500;
+    }
+
+    private double tuition;
+
+    public Student(int studentID, String name, int age) {
+        super(name, age);
+        this.studentID = studentID;
+    }
+
+    public void enrollCourses(List<String>enrolledCourses){
+        this.enrolledCourses = enrolledCourses;
+    }
+
+    public void enrollCourses(String enrolledCourse){
+        this.enrolledCourses.add(enrolledCourse);
+    }
+}
