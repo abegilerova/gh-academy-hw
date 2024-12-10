d9ed5dfcd36dd25b50f9ad838fa2ea2dff71e90c
Aikanysh Begilerova <abegilerova@gmail.com>
2024-12-09 22:44:59 -0600

    wip rest api tutorial

--- computer-systems-and-industrial-programming/week10/crossfit-wod-api/src/index.js
+++ computer-systems-and-industrial-programming/week10/crossfit-wod-api/src/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
-const v1Router = require("./v1/routes");
+//const v1Router = require("./v1/routes");
+
+const bodyParser = require("body-parser");
+const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 
 const app = express();
 
@@ -11,7 +14,9 @@ const PORT = process.env.PORT || 3000;
 // });
 
 // *** ADD ***
-app.use("/api/v1", v1Router);
+// app.use("/api/v1", v1Router);
+app.use(bodyParser.json());
+app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => {
     console.log(`Api is listening on a port ${PORT}`)
