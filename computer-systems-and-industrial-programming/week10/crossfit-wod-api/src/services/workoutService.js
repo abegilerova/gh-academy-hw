4ad611b8c9b408c2906cc9697ba53a7bc5855b1a
Aikanysh Begilerova <abegilerova@gmail.com>
2024-12-10 22:13:30 -0600

    wip

--- computer-systems-and-industrial-programming/week10/crossfit-wod-api/src/services/workoutService.js
+++ computer-systems-and-industrial-programming/week10/crossfit-wod-api/src/services/workoutService.js
@@ -1,4 +1,5 @@
 // In src/services/workoutService.js
+const { v4: uuid } = require("uuid");
 const Workout = require('../database/Workout');
 
 const getAllWorkouts = () => {
@@ -7,11 +8,18 @@ const getAllWorkouts = () => {
 
   };
   
-  const getOneWorkout = () => {
-    return;
+  const getOneWorkout = (workoutId) => {
+    const workout = Workout.getOneWorkout(workoutId);
+    return workout;
   };
   
-  const createNewWorkout = () => {
+  const createNewWorkout = (newWorkout) => {
+    const workoutToInsert = {
+      ...newWorkout,
+      id: uiid(),
+      createdAt: new Date().toLocaleString("en-US",{timeZone: "UTC"}),
+      updatedAt: new Date().toLocaleString("en-US", {timeZone: 'UTC'}),
+    };
     return;
   };
   
