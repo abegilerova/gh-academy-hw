09be574f21e5221ef82423917543c7ebcf8dcf0c
Aikanysh Begilerova <abegilerova@gmail.com>
2024-12-12 23:29:33 -0600

    wip

--- discrete_math_algorithms/week1/BuyAndSellStock.java
+++ discrete_math_algorithms/week1/BuyAndSellStock.java
@@ -6,16 +6,27 @@ import java.util.stream.Collectors;
 
 class Solution {
     public static int maxProfit(int[] prices) {
-        List<Integer> pricesList = Arrays.stream(prices).boxed().collect(Collectors.toList());
-        int min = Arrays.stream(prices).min().getAsInt();
-        int minIndex = pricesList.indexOf(min);
-        if(minIndex == pricesList.size()-1){
-            return 0;
+//        List<Integer> pricesList = Arrays.stream(prices).boxed().collect(Collectors.toList());
+//        int min = Arrays.stream(prices).min().getAsInt();
+//        int minIndex = pricesList.indexOf(min);
+//        if(minIndex == pricesList.size()-1){
+//            return 0;
+//        }
+//        List <Integer> newPricesList = pricesList.subList(minIndex + 1, pricesList.size()-1);
+//        int maxNumber = (int) Collections.max(newPricesList);
+//        System.out.println("maxNumber - min " + (maxNumber - min));
+//        return maxNumber - min;
+        int lowest_price_so_far = prices[0];
+        int highest_profit = 0;
+
+        for (int p : prices){
+            int profit = (p - lowest_price_so_far);
+
+            highest_profit = Math.max(highest_profit, profit);
+            lowest_price_so_far = Math.min(lowest_price_so_far, p);
         }
-        List <Integer> newPricesList = pricesList.subList(minIndex + 1, pricesList.size()-1);
-        int maxNumber = (int) Collections.max(newPricesList);
-        System.out.println("maxNumber - min " + (maxNumber - min));
-        return maxNumber - min;
+
+        return highest_profit;
 
     }
 
