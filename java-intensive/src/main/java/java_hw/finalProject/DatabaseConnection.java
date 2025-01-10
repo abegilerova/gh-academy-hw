73365a990ca2d8b4f7579e942e6ad6a692406b68
Aikanysh Begilerova <abegilerova@gmail.com>
2025-01-09 21:45:17 -0600

    final project wip

---
+++ java-intensive/src/main/java/java_hw/finalProject/DatabaseConnection.java
@@ -0,0 +1,24 @@
+package java_hw.finalProject;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseConnection {
+    private static final String DB_URL = "jdbc:sqlite:/Users/aikanysh/growthhungry/gh-academy-hw/java-intensive/src/main/java/java_hw/finalProject/chat.db";
+
+    static void initializeDatabase (){
+        try(Connection connection = DriverManager.getConnection(DB_URL);
+            Statement stmt = connection.createStatement()){
+            String userTable = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE);";
+            String messageTable = "CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, message TEXT, " +
+                    "timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);";
+            stmt.execute(userTable);
+            stmt.execute(messageTable);
+
+        } catch (SQLException e) {
+            System.err.println("Error initializing database: " + e.getMessage());
+        }
+    }
+}
