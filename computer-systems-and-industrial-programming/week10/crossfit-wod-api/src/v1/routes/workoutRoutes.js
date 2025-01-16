638a4141ec7594f6c62c6f18847e4de17cb091e1
Aikanysh Begilerova <abegilerova@gmail.com>
2025-01-16 14:34:32 -0600

    cache is done

--- computer-systems-and-industrial-programming/week10/crossfit-wod-api/src/v1/routes/workoutRoutes.js
+++ computer-systems-and-industrial-programming/week10/crossfit-wod-api/src/v1/routes/workoutRoutes.js
@@ -1,9 +1,23 @@
 // In src/v1/routes/workoutRoutes.js
 const express = require("express");
+const bodyParser = require("body-parser");
+const apicache = require("apicache");
+
+
 const workoutController = require("../../controllers/workoutController");
 const recordController = require("../../controllers/recordController");
 
 const router = express.Router();
+
+const app = express();
+const cache = apicache.middleware;
+
+app.use(bodyParser.json());
+
+app.use(cache("2 minutes"));
+// app.use("/api/v1/workouts", v1WorkoutRouter);
+
+router.get("/", cache("2 minutes"), workoutController.getAllWorkouts);
 
 router.get("/", workoutController.getAllWorkouts);
 
