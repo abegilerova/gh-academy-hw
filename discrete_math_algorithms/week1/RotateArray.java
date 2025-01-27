de9644debbacede1ce71a1ea9c0c616cc9d46156
Aikanysh Begilerova <abegilerova@gmail.com>
2025-01-26 22:21:34 -0600

    rotate is done

--- discrete_math_algorithms/week1/RotateArray.java
+++ discrete_math_algorithms/week1/RotateArray.java
@@ -1,46 +1,30 @@
+import java.awt.desktop.PreferencesEvent;
 import java.util.Arrays;
 
 public class RotateArray {
     public static void rotate(int[] nums, int k) {
-        int left = 0;
-        int right = nums.length -1;
-        int copyK = k;
-
-        while (left <= right){
-            int temp = nums[left];
-            nums[left] = nums[right];
-            nums[right] = temp;
-            left++;
-            right--;
-        }
-
-        int nextLeft = 0;
-
-
-        while (nextLeft <= k){
-            int temp = nums[nextLeft];
-            nums[nextLeft] = nums[k-1];
-            nums[k -1] = temp;
-            nextLeft++;
-            k--;
-        }
-
-
-        int nextRight = nums.length - 1;
-        while(copyK <= nextRight ){
-            int temp = nums[copyK];
-            nums[copyK] = nums[nextRight];
-            nums[nextRight] = temp;
-            copyK++;
-            nextRight--;
-        }
-
+        k = k % nums.length  ;
+        reverse(nums, 0, nums.length-1);
+        reverse(nums,0, k-1 );
+        reverse(nums, k, nums.length-1);
         System.out.println(Arrays.toString(nums));
 
     }
 
+    public static void reverse(int[] nums, int start, int end){
+        while (start < end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
     public static void main(String[] args) {
         int [] nums = {1,2,3,4,5,6,7};
+        //            {7,6,5,4,3,2,1}
+        //output =    [5,6,7,1,2,3,4]
         int k = 3;
 
         rotate(nums, k);
