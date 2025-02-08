bbaa549824531b2f5c3f70dee668e8c679f1c9d2
Aikanysh Begilerova <abegilerova@gmail.com>
2025-02-07 23:36:45 -0600

    topkfrequent

---
+++ discrete_math_algorithms/week1/TopKFrequentElements.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class TopKFrequentElements {
+    public static int[] topKFrequent(int[] nums, int k) {
+
+        int [] finalResult = new int [k];
+        Map<Integer, Integer> freqMap = new HashMap<>();
+
+        for (int num : nums){
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+       //System.out.println(freqMap);
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(Comparator.comparingInt(freqMap::get));
+
+        System.out.println(minHeap);
+        for (int num : freqMap.keySet()){
+            System.out.println(num);
+            minHeap.offer(num);
+            if (minHeap.size() > k){
+                minHeap.poll();
+            }
+
+        }
+
+       System.out.println(minHeap);
+
+        return finalResult;
+    }
+
+    public static void main(String[] args) {
+        int [] nums = {1,1,1,2,2,3};
+        int k = 2;
+
+        topKFrequent(nums, k);
+
+    }
+}
