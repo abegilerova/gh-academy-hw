db59ce5b3a9e40ee8ddfdfe61709e5e023482721
Aikanysh Begilerova <abegilerova@gmail.com>
2025-02-12 17:47:47 -0600

    done

--- discrete_math_algorithms/week1/hashMap/LongestConsecutiveSeq.java
+++ discrete_math_algorithms/week1/hashMap/LongestConsecutiveSeq.java
@@ -13,10 +13,12 @@ public class LongestConsecutiveSeq {
         int longest = 0;
 
         for (int num : numSet){
-            System.out.println("num " + num);
+//            System.out.println("num " + num);
             if(!numSet.contains(num -1)){
                 int currentNum = num;
                 int currentStreak = 1;
+
+                System.out.println("currentNum " + currentNum);
 
                 while(numSet.contains(currentNum + 1)){
                     currentNum++;
@@ -24,11 +26,13 @@ public class LongestConsecutiveSeq {
 
                 }
 
+                System.out.println("currentStreak " + currentStreak);
                 longest = Math.max(longest, currentStreak);
+                System.out.println(longest);
             }
         }
 
-        System.out.println(longest);
+//        System.out.println(longest);
         return longest;
     }
 
@@ -36,35 +40,5 @@ public class LongestConsecutiveSeq {
         int [] nums =  {100,4,200,1,3,2};
         longestConsecutive(nums);
 
-    }
-
-    public static class GroupAnagrams {
-
-        public  static List<List<String>> groupAnagrams(String[] strs) {
-            Map<String, List<String>> finalMap = new HashMap<>();
-
-            for (String str : strs){
-                char [] charArray = str.toCharArray();
-                Arrays.sort(charArray);
-                String sortedStr = new String(charArray);
-                if(!finalMap.containsKey(sortedStr)){
-                    finalMap.put(sortedStr, new ArrayList<>());
-                    System.out.println(finalMap);
-                }
-
-                finalMap.get(sortedStr).add(str);
-                System.out.println(finalMap);
-            }
-
-            return new ArrayList<>(finalMap.values());
-
-        }
-
-        public static void main(String[] args) {
-
-            String [] strs = {"eat","tea","tan","ate","nat","bat"};
-            groupAnagrams(strs);
-
-        }
     }
 }
