e812a1a6f12620cfeb43d7471846d57f9debdfbc
Aikanysh Begilerova <abegilerova@gmail.com>
2025-03-07 23:56:35 -0600

    done

--- discrete_math_algorithms/week1/backtracking/GenerateParentheses.java
+++ discrete_math_algorithms/week1/backtracking/GenerateParentheses.java
@@ -6,24 +6,33 @@ import java.util.List;
 public class GenerateParentheses {
     public static List<String> generateParenthesis(int n) {
         List<String> result = new ArrayList<>();
-        backTrack(0, n, result);
+
+        backTrack(result, new StringBuilder(),0,0, n);
         System.out.println(result);
         return result;
     }
 
-    public static void backTrack(int start,int n, List<String> result){
+    public static void backTrack( List<String> result, StringBuilder combination, int openCount, int closeCount, int n) {
 
-        String combination = "";
         //baseCase
-        if(combination.length() == 2 * n){
-            result.add(combination);
+        if (combination.length() == 2 * n) {
+            result.add(combination.toString());
             return;
-
         }
 
+        for (int i = 0; i < 2; i++) {
+            if (i == 0 && openCount < n) {
+                combination.append("(");
+                backTrack(result, combination, openCount + 1, closeCount, n);
+                combination.deleteCharAt(combination.length() - 1);
+            }
 
-
-
+            if (i == 1 && closeCount < openCount) {
+                combination.append(")");
+                backTrack(result, combination, openCount, closeCount + 1, n);
+                combination.deleteCharAt(combination.length() - 1);
+            }
+        }
     }
 
     public static void main(String[] args) {
