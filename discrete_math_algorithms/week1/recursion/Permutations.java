244fea6bb09f5a906d29b32f5d3d4925f9a75d82
Aikanysh Begilerova <abegilerova@gmail.com>
2025-03-12 18:14:07 -0500

    done

--- discrete_math_algorithms/week1/recursion/Permutations.java
+++ discrete_math_algorithms/week1/recursion/Permutations.java
@@ -9,30 +9,46 @@ public class Permutations {
 
         List<List<Integer>> result = new ArrayList<>();
 
-        backtrack(nums,0, new ArrayList<>(), result );
-
-        System.out.println(result);
+        backtrack(nums, new ArrayList<>(), new boolean[nums.length], result);
 
         return result;
-
     }
 
-    public static void backtrack(int [] nums, int start, List<Integer> combination,  List<List<Integer>> result){
-        if(combination.size() == nums.length){
-            result.add(combination);
+    public static void backtrack(int[] nums, List<Integer> combination, boolean[] used, List<List<Integer>> result) {
+        // Print the current combination at the start of the recursive call
+        System.out.println("Current combination: " + combination);
+
+        if (combination.size() == nums.length) {
+            // When we find a valid permutation, print it before adding it to the result
+            System.out.println("Found permutation: " + combination);
+            result.add(new ArrayList<>(combination));
             return;
         }
 
-        for (int i = start; i < nums.length; i++){
-            combination.add(nums[i]);
-            backtrack(nums, start + 1, combination, result);
-            combination.remove(combination.size()-1);
-        }
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) {
+                continue;  // Skip used elements
+            }
 
+            // Print the decision to include the current element
+            System.out.println("Choosing " + nums[i] + " at index " + i);
+
+            combination.add(nums[i]);  // Add the element to the combination
+            used[i] = true;            // Mark this element as used
+
+            // Recursive call to build the next level of the combination
+            backtrack(nums, combination, used, result);
+
+            // After backtracking, print the state of the combination
+            System.out.println("Backtracking, removing " + nums[i] + " from combination");
+
+            combination.remove(combination.size() - 1);  // Remove the last element (backtrack)
+            used[i] = false;  // Mark this element as unused for the next round
+        }
     }
 
     public static void main(String[] args) {
-       int[] nums = {1,2,3};
-       permute(nums);
+        int[] nums = {1, 2, 3};
+        permute(nums);
     }
 }
