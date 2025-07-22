86cc82977dfde921b6f313fd1392368fd8881e29
Aikanysh Begilerova <abegilerova@gmail.com>
2025-07-22 10:55:02 -0500

    file based db is done

---
+++ computer-systems-and-industrial-programming/fileHandling/untitled/src/main/java/org/example/fileBasedDatabase/User.java
@@ -0,0 +1,4 @@
+package org.example.fileBasedDatabase;
+
+public class User {
+}
---
+++ computer-systems-and-industrial-programming/fileHandling/untitled/src/main/java/org/example/fileBasedDatabase/UserRepository.java
@@ -0,0 +1,5 @@
+package org.example.fileBasedDatabase;
+
+public class UserRespository {
+    private final String filePath = "users.db";
+}
